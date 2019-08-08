6feb5a0bb650357b5971bdacb73cb570a72bbb65
Basuki Winoto <basuki.winoto@logmein.com>
2019-08-08 11:18:14 -0400

    Added tracker

--- scripts/extra.js
+++ scripts/extra.js
@@ -3,6 +3,25 @@ document.addEventListener('DOMContentLoaded', resetStorage)
 document.addEventListener('DOMContentLoaded', logHit)
 document.addEventListener('DOMContentLoaded', outboundClicks)
 document.addEventListener('DOMContentLoaded', logFeaturesSelection)
+document.addEventListener('DOMContentLoaded', trackLogin)
+
+function trackLogin() {
+  const loginButton = document.querySelector('a.nav-log-in', loginClicked)
+  loginButton.addEventListener('click', loginClicked)
+
+  const callback = function () {
+    if (typeof(url) != 'undefined') {
+      window.location = url;
+    }
+  };
+
+  function loginClicked() {
+    gtag('event', 'conversion', {
+        'send_to': 'AW-776914703/vzRPCMLYuacBEI-Ou_IC',
+        'event_callback': callback
+    });
+  }
+}
 
 function logFeaturesSelection() {
     function uuidv4() {
@@ -23,6 +42,7 @@ function logFeaturesSelection() {
       postData.size = (localStorage.getItem('selectedSegment'))?localStorage.getItem('selectedSegment'):''
       postData.products = (localStorage.getItem('selectedProducts'))?localStorage.getItem('selectedProducts').split(',').filter((el)=>el!==''):[]
       postData.addons = (localStorage.getItem('selectedAddons'))?localStorage.getItem('selectedAddons').split(',').filter((el)=>el!==''):[]
+      postData.visitor = getCookie('visitor')
 
       // send to google sheet
       let urlParameters = Object.entries(postData).map(e => e.join('=')).join('&');
@@ -37,6 +57,31 @@ function logFeaturesSelection() {
     })
     const recomendationButton = document.querySelector('.recomendation .recomendation__button')
     recomendationButton.addEventListener('click', logIt.bind(null, postData, '1'))
+
+    let recommendationSection = document.querySelector('.recomendation')
+    recommendationSection.addEventListener('click', function(e){
+        sectionClicked(e, postData)
+    })
+
+    function sectionClicked(e, postData) {
+        const element = e.target
+        if (element.className.startsWith('option-list')) {
+            logIt(postData, '0')
+        }
+        if (element.tagName==='LI') {
+            logIt(postData, '0')
+        }
+        if(element.matches(`li ${element.tagName}`)){
+            let p = element.parentNode
+            while(p.tagName!=='LI' && p.tagName!=='SECTION') {
+                p = p.parentNode
+            }
+            if (p.tagName==='LI') {
+                logIt(postData, '0')
+            }
+        }
+    }
+
 }
 
 
@@ -59,7 +104,12 @@ function outboundClick() {
 
 
 function logHit() {
-    fetch('https://us-central1-bundleit-02210.cloudfunctions.net/api/logHit')
+    fetch('https://us-central1-bundleit-02210.cloudfunctions.net/api/logHit').then((res)=>{
+        return res.json()
+    }).then((data)=>{
+        let userAgent = JSON.parse(data.result)
+        setCookie('visitor',`${userAgent['x-appengine-user-ip']}/${userAgent['x-forwarded-for']}`,1)
+    })
 }
 
 function resetStorage() {
@@ -76,11 +126,9 @@ function slackEventNotification() {
         buttons[key].addEventListener('click', buttonClicked)
     })
 
-    let segments = document.querySelectorAll('.segment a')
-    Object.keys(segments).map((key)=>{
-        segments[key].removeEventListener('click', buttonClicked)
-        segments[key].addEventListener('click', buttonClicked)
-    })
+    let recommendationSection = document.querySelector('.recomendation')
+    recommendationSection.removeEventListener('click', sectionClicked)
+    recommendationSection.addEventListener('click', sectionClicked)
 
     let contactButton = document.querySelector('#build-bundle .contact-me .contact')
     contactButton.removeEventListener('click', contactButtonClicked)
@@ -98,6 +146,25 @@ function slackEventNotification() {
         }
     }
 
+    function sectionClicked(e) {
+        const element = e.target
+        if (element.className.startsWith('option-list')) {
+            sendToSlack(`${emoji} | ${element.innerText}`)
+        }
+        if (element.tagName==='LI') {
+            sendToSlack(`${emoji} | ${element.getAttribute('data-product')}`)
+        }
+        if(element.matches(`li ${element.tagName}`)){
+            let p = element.parentNode
+            while(p.tagName!=='LI' && p.tagName!=='SECTION') {
+                p = p.parentNode
+            }
+            if (p.tagName==='LI') {
+                sendToSlack(`${emoji} | ${p.getAttribute('data-product')}`)
+            }
+        }
+    }
+
     function sendToSlack(message) {
       fetch('https://hooks.slack.com/services/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', {
           method: 'post',
@@ -108,4 +175,27 @@ function slackEventNotification() {
           body: JSON.stringify({text: message})
       })
     }
+}
+
+function getCookie(cname) {
+  var name = cname + '=';
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return '';
+}
+
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  var expires = 'expires='+ d.toUTCString();
+  document.cookie = cname + '=' + cvalue + ';' + expires + ';path=/';
 }
